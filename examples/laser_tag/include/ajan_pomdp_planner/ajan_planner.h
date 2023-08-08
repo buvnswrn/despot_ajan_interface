2d60a3b626631e7762f8e65734a7469f22c4eefe
Bhuvaneshwaran I <bhuvan.nav@gmail.com>
2023-08-08 20:20:48 +0200

    Added Planner class

---
+++ examples/laser_tag/include/ajan_pomdp_planner/ajan_planner.h
@@ -0,0 +1,17 @@
+//
+// Created by bhuvanesh on 08.08.23.
+//
+#include "de_dfki_asr_ajan_pluginsystem_mdpplugin_utils_POMDP_DESPOT_implementation_AjanPlanner.h"
+#ifndef LASER_TAG_AJAN_PLANNER_H
+#define LASER_TAG_AJAN_PLANNER_H
+#include <string>
+#include <despot/planner.h>
+
+using namespace despot;
+class AjanPlanner : public Planner{
+
+
+};
+
+
+#endif //LASER_TAG_AJAN_PLANNER_H
---
+++ examples/laser_tag/src/ajan_pomdp_planner/cpp/ajan_planner.cpp
@@ -0,0 +1,57 @@
+//
+// Created by bhuvanesh on 08.08.23.
+//
+
+
+#include <despot/planner.h>
+#include "de_dfki_asr_ajan_pluginsystem_mdpplugin_utils_POMDP_DESPOT_implementation_AjanPlanner.h"
+#include "ajan_agent.h"
+#include "ajan_world.h"
+
+using namespace despot;
+using namespace std;
+
+class AjanPlanner : public Planner {
+
+public:
+    DSPOMDP* InitializeModel(option::Option* options) override {
+        // Initialize POMDP Model here;
+        DSPOMDP* model = new AjanAgent();
+        return model;
+    }
+
+    World* InitializeWorld(std::string& world_type, DSPOMDP* model, option::Option* options) override {
+        // TODO: Make JNI Call to get World Type and Instantiate it.
+
+        if(world_type == "pomdp") {
+            return InitializePOMDPWorld(world_type, model, options);
+        } else {
+            cout<<"Create a world as defined and implemented by the user"<<endl;
+            AjanWorld* world = new AjanWorld();
+            cout<<"Establish connection with external system"<<endl;
+            world->Connect();
+            cout<<"Initialize the state of the external system"<<endl;
+            world->Initialize();
+            cout<<"Initialized"<<endl;
+//            static_cast<AJANAgent*>(model)->NoiseSigma(world->noise_sigma_);
+            cout<<"Assigned Noise sigma"<<endl;
+            return world;
+        }
+    }
+
+    void InitializeDefaultParameters() override {
+        Globals::config.pruning_constant = 0.01;
+    }
+
+    std::string ChooseSolver() override {
+        // TODO: Make JNI Call to get Solver name
+        return "DESPOT";
+    }
+};
+
+JNIEXPORT jint JNICALL Java_de_dfki_asr_ajan_pluginsystem_mdpplugin_utils_POMDP_DESPOT_implementation_AjanPlanner_RunPlanner
+        (JNIEnv * env, jobject thisObject, jobject agentObject, jobject worldObject) {
+    char* argv[] = {strdup("AJAN_Planner") };
+    cout<<"Starting the AJAN-DESPOT Planner" <<endl;
+    return AjanPlanner().RunEvaluation(1, argv);
+}
