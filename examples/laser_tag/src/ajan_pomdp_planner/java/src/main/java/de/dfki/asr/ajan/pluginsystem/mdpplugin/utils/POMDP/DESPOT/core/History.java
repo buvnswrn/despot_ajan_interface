06b98f368b124139f17927d82ba6a1580cfdc930
Bhuvaneshwaran I <bhuvan.nav@gmail.com>
2023-08-10 13:08:42 +0200

    Added History variables

--- examples/laser_tag/src/ajan_pomdp_planner/java/src/main/java/de/dfki/asr/ajan/pluginsystem/mdpplugin/utils/POMDP/DESPOT/core/History.java
+++ examples/laser_tag/src/ajan_pomdp_planner/java/src/main/java/de/dfki/asr/ajan/pluginsystem/mdpplugin/utils/POMDP/DESPOT/core/History.java
@@ -1,4 +1,9 @@
 package de.dfki.asr.ajan.pluginsystem.mdpplugin.utils.POMDP.DESPOT.core;
 
+import java.util.ArrayList;
+import java.util.Vector;
+
 public class History {
+    Vector<Integer> actions_;
+    Vector<Long> observations_;
 }
