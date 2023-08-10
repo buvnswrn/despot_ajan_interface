320c8aa10b660c952ed42c764be77c6039db5abe
Bhuvaneshwaran I <bhuvan.nav@gmail.com>
2023-08-10 13:04:00 +0200

    Added Cache Method Names

--- examples/laser_tag/src/ajan_pomdp_planner/cpp/ajan_jni_cache_methods.cpp
+++ examples/laser_tag/src/ajan_pomdp_planner/cpp/ajan_jni_cache_methods.cpp
@@ -52,6 +52,9 @@ void AjanHelper::GetAllMethodID() {
     GetAllAjanPolicyMethodID();
 }
 
+/**
+ * Corresponding Java Class: AjanPlanner.java
+ */
 void AjanHelper::GetAllPlannerMethodID() {
 
     cout << "Initializing of Planner methods" << std::endl;
@@ -59,7 +62,6 @@ void AjanHelper::GetAllPlannerMethodID() {
     string methodNames[totalMethod][2] = {
             {"InitializeModel", "()Z"},
             {"InitializeWorld", "(" + getSig(STRING) + ")Z",},
-            {"PrintMethod",     "()V"},
             {"ChooseSolver",    "()" + getSig(STRING)},
             {"getWorldType",    "()" + getSig(STRING)}
     };
@@ -72,40 +74,62 @@ void AjanHelper::GetAllPlannerMethodID() {
 
 }
 
+/**
+ * Corresponding Java Class: AjanAgent.java
+ */
 void AjanHelper::GetAllAgentMethodID() {
     cout << "Initializing of Agent methods" << std::endl;
     const int totalMethod = 30;
     string methodNames[totalMethod][2] = {
-            {"Step",                      "(" + getSig(AJAN_AGENT_STATE) + "DIDI)Z"},
-            {"NumActions",                "()I"},
+            //region  MDP Functions
             {"NumStates",                 "()I"},
-            {"ObsProb",                   "(I" + getSig(AJAN_AGENT_STATE) + "I)D"},
-            {"CreateStartState",          "(" + getSig(STRING) + ")" + getSig(AJAN_AGENT_STATE)},
-            {"getInitialBeliefParticles", "(" + getSig(STATE) + getSig(STRING) + ")" + getSig(VECTOR)},
-            {"GetMaxReward",              "()D"},
-//                {"Reward","("+ getSig(STATE)+"I)D"},
-            {"Reward",                    "(II)D"},
-            {"GetBestAction",             "()" + getSig(VALUED_ACTION)},
-            {"GetAction",                 "(" + getSig(STATE) + ")I"},
-            {"CreateScenarioLowerBound",  "(" + getSig(STRING) + getSig(STRING) + ")" + getSig(AJAN_POLICY)},
-            {"CreateScenarioUpperBound",  "("+ getSig(STRING)+ getSig(STRING)+")"+ getSig(STRING)},
-            {"CreateParticleUpperBound",  "("+ getSig(STRING)+")"+ getSig(AJAN_PARTICLE_UPPER_BOUND)},
-            {"CreateParticleLowerBound",  "("+ getSig(STRING)+")"+ getSig(STRING)},
+            {"NumActions",                "()I"},
             {"TransitionProbability",     "(II)" + getSig(VECTOR)},
-            {"GetIndex",                  "("+ getSig(STATE)+")I"},
+            {"Reward",                    "(II)D"},
+            //endregion
+            //region Belief MDP
+            {"CreateBeliefLowerBound",  "(" + getSig(STRING) + ")" + getSig(AJAN_BELIEF_POLICY)}, // WARN: Need to check whether BeliefLowerBound class can be created. Can throw error here.
+            {"CreateBeliefUpperBound",  "(" + getSig(STRING) + ")" + getSig(AJAN_UPPER_BOUND)},
+            {"Tau", "(" + getSig(AJAN_BELIEF)+ "IJ)" + getSig(BELIEF)}, // WARN:This may not work
+            //            {"getTauParticles",           "(" + getSig(VECTOR) + "II)" + getSig(VECTOR)}, // Not available till now but can be created
+            {"Observe",             "()" + getSig(AJAN_AGENT)}, // see what is it for map
+            {"StepReward","("+ getSig(BELIEF)+"I)"+"D"},
+//            {"StepRewardFromParticles",             "("+ getSig(VECTOR)+"I)D"},
+//            {"StepRewardFromParticles",   "(" + getSig(VECTOR) + "I)D"},
+            //endregion
+            //region StateIndexer
+            {"GetIndex",                  "("+ getSig(STATE)+")I"}, // WARN: State is an abstract class, see whether AjanAgentState can be used instead
             {"GetState",                  "(I)"+ getSig(STATE)},
+            //endregion
+            //region StatePolicy
+            {"GetAction",                 "(" + getSig(STATE) + ")I"},
+            //endregion
+            //region MMAPInferencer
+            {"GetMMAP",                   "(" + getSig(VECTOR) + ")" + getSig(AJAN_AGENT_STATE)},
+            //endregion
+            //region POMDP
+            {"Step",                      "(" + getSig(AJAN_AGENT_STATE) + "DIDJ)Z"},
+//            {"Step",                      "(" + getSig(AJAN_AGENT_STATE) + "DID)Z"}, // not possible to call by same name according to this method
+//            {"Step",                      "(" + getSig(AJAN_AGENT_STATE) + "IDJ)Z"}, // not possible to call by same name according to this method
+//                {"Reward","("+ getSig(STATE)+"I)D"}, // cached in region MDP with other params. not possible to call by same name according to this method. Call directly
+            {"ObsProb",                   "(J" + getSig(AJAN_AGENT_STATE) + "I)D"},
+            {"CreateStartState",          "(" + getSig(STRING) + ")" + getSig(AJAN_AGENT_STATE)}, // WARN: Return type is State
+            {"InitialBelief","("+ getSig(STATE)+ getSig(STRING)+")"+ getSig(BELIEF)},// WARN: Belief and State are abstract classes - Might not be possible to create objects
+//            {"getInitialBeliefParticles", "(" + getSig(STATE) + getSig(STRING) + ")" + getSig(VECTOR)},
+            {"GetMaxReward",              "()D"},
+            {"GetBestAction",             "()" + getSig(VALUED_ACTION)},
+            {"CreateParticleUpperBound",  "("+ getSig(STRING)+")"+ getSig(AJAN_PARTICLE_UPPER_BOUND)},// WARN: Return type is Particle Upperbound which is abstract
+            {"CreateScenarioUpperBound",  "("+ getSig(STRING)+ getSig(STRING)+")"+ getSig(STRING)}, // WARN: This does not exist as of now
+            {"CreateParticleLowerBound",  "("+ getSig(STRING)+")"+ getSig(STRING)}, // WARN: This does not exist as of now
+            {"CreateScenarioLowerBound",  "(" + getSig(STRING) + getSig(STRING) + ")" + getSig(AJAN_POLICY)}, // WARN: Return type is ScenarioLowerBound
             {"PrintState",                "(" + getSig(STATE) + ")V"},
-            {"PrintObs",                  "(" + getSig(STATE) + "I)V"},
+            {"PrintObs",                  "(" + getSig(STATE) + "J)V"},
             {"PrintAction",               "(I)V"},
             {"PrintBelief",               "(" + getSig(BELIEF) + ")V"},
-            {"GetMMAP",                   "(" + getSig(VECTOR) + ")" + getSig(AJAN_AGENT_STATE)},
-            {"getTauParticles",           "(" + getSig(VECTOR) + "II)" + getSig(VECTOR)},
-            {"Observe",             "()" + getSig(AJAN_AGENT)}, // see what is it for map
-            {"StepRewardFromParticles",             "("+ getSig(VECTOR)+"I)D"},
 //            {"Observe","("+ getSig()+"II)"+ getSig()},
-            {"StepRewardFromParticles",   "(" + getSig(VECTOR) + "I)D"},
-            {"PrintMethod",               "()V"},
-
+            {"NumActiveParticles","()I"},
+//            {"PrintMethod",               "()V"},
+            //endregion
     };
 
     for (auto &methodName: methodNames) {
@@ -117,6 +141,9 @@ void AjanHelper::GetAllAgentMethodID() {
 
 }
 
+/**
+ * Corresponding Java Class: AjanAgentState.java
+ */
 void AjanHelper::GetAllStateMethodID() {
     cout << "Initializing the State methods" << std::endl;
     const int totalMethod = 2;
@@ -132,16 +159,19 @@ void AjanHelper::GetAllStateMethodID() {
     cout << "Initialization of State methods Complete" << std::endl;
 }
 
+/**
+ * Corresponding Java Class: AjanWorld.java
+ */
 void AjanHelper::GetAllWorldMethodID() {
     cout << "Initializing the World methods" << std::endl;
     const int totalMethod = 6;
     string methodNames[totalMethod][2] = {
             {"Connect",               "()Z"},
+            {"Initialize",            "()" + getSig(STATE)},
+            {"GetCurrentState",       "()" + getSig(STATE)},
             {"ExecuteAction",         "(II)Z",},
             {"getCurrentObservation", "()J"},
-            {"setCurrentObservation", "(J)Z"},
-            {"Initialize",            "()" + getSig(STATE)},
-            {"GetCurrentState",       "()" + getSig(STATE)}
+            {"setCurrentObservation", "(J)Z"}
     };
 
     for (auto &methodName: methodNames) {
@@ -170,11 +200,15 @@ void AjanHelper::GetAllVectorMethodID() {
     cout << "Initialization of Vector methods Complete" << std::endl;
 }
 
+/**
+ * Corresponding Java Class: AjanParticleUpperBound.java
+ */
 void AjanHelper::GetAllParticleUpperBoundMethodID() {
     cout << "Initializing the ParticleUpperBound methods" << std::endl;
     const int totalMethod = 1;
     string methodNames[totalMethod][2] = {
-            {"Value", "(I)D"}
+//            {"Value", "(I)D"},
+            {"Value", "("+ getSig(STATE)+")D"}
     };
     for (auto &methodName: methodNames) {
         particleUpperBoundMethods[methodName[0]] = (methodName[0], getEnv()->GetMethodID(getParticleUpperBoundClass(),
@@ -184,12 +218,16 @@ void AjanHelper::GetAllParticleUpperBoundMethodID() {
     cout << "Initialization of ParticleUpperBound methods Complete" << std::endl;
 }
 
+/**
+ * Corresponding Java Class: AjanPolicy.java -> DefaultPolicy -> ScenarioLowerBound
+ */
 void AjanHelper::GetAllAjanPolicyMethodID() {
     cout << "Initializing the AJAN Policy methods" << std::endl;
     const int totalMethod = 2;
     string methodNames1[totalMethod][2] = {
-            {"Action", "(" + getSig(VECTOR) + "J)I",},
-            {"TestMethod", "()I"}
+//            {"Action", "(" + getSig(VECTOR) + "J)I",},
+            {"Action", "(" + getSig(VECTOR) + getSig(HISTORY)+")I"}
+//            {"TestMethod", "()I"}
     };
 
     for (auto &methodName: methodNames1) {
